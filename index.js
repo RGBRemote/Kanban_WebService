01d357e29c70fa7fe7ade42b9d5533056a202965
RGBRemote <remotergb8@gmail.com>
2025-04-23 10:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const kanbanRoutes = require('./routes/kanban.routes');
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', '*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
